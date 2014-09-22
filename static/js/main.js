d9d76bcceff9dd87f32035aa3d76e23230587d7e
NateLassiter <n8ln8l@gmail.com>
2014-09-22 17:34:07 +0200

    Recos Page
    Note: Still needs to be reviewed by Aasim before Integration.

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,10 @@
 $("#contact-info-btn").click (function(){
 	$('.contact-cover').addClass('animated fadeOutDown');
 });
+
+// RECOS
+
+$("#show-more-recos").click (function (){
+	$(this).toggleClass("hide");
+	$(".more-recos").toggleClass("hide").addClass("animated fadeIn");
+});
