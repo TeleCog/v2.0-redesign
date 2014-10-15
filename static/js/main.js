839efde7008ae4c284d7fff3cac5d44e2fb0e581
NateLassiter <n8ln8l@gmail.com>
2014-10-15 16:37:12 +0200

    Homepage animation v4

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,7 @@ $("#show-more-recos").click (function (){
 // CAROUSEL
 
 $('.carousel').carousel({
-	interval: 3500,
+	interval: 2500,
 	wrap: false
 });
 
@@ -36,17 +36,55 @@ $('#profile-cover2').waypoint(function() {
 	$(".caption2").addClass('covered');
 });
 
+$('#how-to-trigger').waypoint(function() {
+	$("#how-to-trigger li").addClass('animated fadeIn ').removeClass('invisible');
+});
+
+$('#makes-it-trigger').waypoint(function() {
+	$("#makes-it-trigger li").addClass('animated fadeIn ').removeClass('invisible');
+});
+
 $('#device-trigger').waypoint(function() {
 	$(".devices-container").addClass('device-moved');
 });
 
-$('#tweet-trigger').waypoint(function() {
-	$(".tweet-txt").addClass('animated fadeInUp show');
+$('#big-deal-trigger').waypoint(function() {
+	$("#big-deal-trigger img.press").addClass('animated fadeInUp ').removeClass('invisible');
 });
 
 $('#chart-trigger').waypoint(function() {
 	$(".bars").addClass('animated fadeInUp show');
 });
+
+
+$('#tweet-trigger').waypoint(function() {
+	$(".tweet-txt").addClass('animated fadeInUp show');
+});
+
+$('.more-tailored').waypoint(function() {
+	$('.more-tailored .img-pulse').addClass('animated pulse');
+}, { offset: '18%' });
+
+$('.how-to-livewire').waypoint(function() {
+	$('.how-to-livewire .img-pulse').addClass('animated pulse');
+}, { offset: '18%' });
+
+$('.makes-it-easy').waypoint(function() {
+	$('.makes-it-easy .img-pulse').addClass('animated pulse');
+}, { offset: '18%' });
+
+$('.big-deal').waypoint(function() {
+	$('.big-deal .img-pulse').addClass('animated pulse');
+}, { offset: '18%' });
+
+$('.people-are-talking').waypoint(function() {
+	$('.people-are-talking .img-pulse').addClass('animated pulse');
+}, { offset: '18%' });
+
+$('.where-is').waypoint(function() {
+	$('.where-is .img-pulse').addClass('animated pulse');
+}, { offset: '18%' });
+
 
 
 // FIXED HEADER SCROLL EFFECT
